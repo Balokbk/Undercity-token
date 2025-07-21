c23415cbc5b61153ae1c72ef9c1583efd4482267
Balo <kawanknupp@gmail.com>
2025-07-20 21:34:35 -0300

    final addings

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+const itens = ['none', 'green-token.png', 'red-token.png', 'redNGreen-token.png']
+const roomsState = {}
+
 function toggleLocation(location){
-    console.log("Toggling location: " + location);
+
+    const room = document.getElementById(location)
+
+    if(!(location in roomsState)){
+        roomsState[location] = 0
+    }
+
+    const existingContainer = room.querySelector('.img-player-container')
+    if(existingContainer){
+        existingContainer.remove()
+    }
+    
+    
+    if(roomsState[location] == 3) {
+        roomsState[location] = 0
+        return
+    } else {
+        roomsState[location]++
+
+        const container = document.createElement('div')
+        container.className = 'img-player-container'
+
+        const img = document.createElement('img')
+        img.src = `images/${itens[roomsState[location]]}`
+        img.className = 'player-token'
+        
+        container.appendChild(img)
+        room.appendChild(container)
+    }
 }
+
+//<div class="img-player-container"><img src="images/green-token.png" class="player-token"></div>
